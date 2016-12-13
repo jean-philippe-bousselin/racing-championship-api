c33a1c15986dd1d9f2361b8d5f97d2c7d71b24f3
jean-philippe-bousselin <jp.bousselin@gmail.com>
2016-12-13 19:50:41 +0100

    Enhanced docker start script

--- start.sh
+++ start.sh
@@ -1,15 +1,33 @@
 
 # start mariadb container
+echo -e "\r\n\e[1m> STARTING MARIADB CONTAINER \e[21m \r\n"
 if [[ "$(docker ps -a | grep my-mariadb | wc -l)" > 0 ]]; then
   docker start my-mariadb
 else
   docker run --name my-mariadb -e MYSQL_ROOT_PASSWORD=passwd -d mariadb:10.1
 fi
 
+# rebuild image if parameter is there
+if [[ $1 == '--rebuild' ]]; then
+  echo -e "\r\n\e[1m> BUILDING API IMAGE  \e[21m \r\n"
+  docker build -t rcm_api .
+fi
+
 # start app container
-docker build -t rcm_api .
+echo -e "\r\n\e[1m> STARTING API CONTAINER \e[21m \r\n"
+docker stop rcm_api_running
 docker rm rcm_api_running
-docker run -it --link my-mariadb:mysql --name="rcm_api_running" -p 80:80 -v $(pwd)/src:/var/www rcm_api
+docker run -d -it --link my-mariadb:mysql --name="rcm_api_running" -p 80:80 -v $(pwd)/src:/var/www rcm_api
+
+echo -e "\r\n\e[1m> CREATING DATABASE... \e[21m \r\n"
+docker exec -it my-mariadb mysql -u root -ppasswd -e "CREATE DATABASE IF NOT EXISTS rcm_api"
+echo 'ok'
+
+echo -e "\r\n\e[1m> EXECUTING MIGRATIONS... \e[21m \r\n"
+docker exec -it rcm_api_running bash -c "cd /var/www/lumen && php artisan migrate"
+
+echo -e "\r\n\e[1m> API IS READY AND RUNNING ON 127.0.0.1 \e[21m \r\n"
+
 
 # Delete all containers
 # docker rm $(docker ps -a -q)
